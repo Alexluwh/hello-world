22d994260364688358c8f6b10bcfcf9616f2cdc2
Alexluwh <35101826+Alexluwh@users.noreply.github.com>
2019-07-07 18:01:56 +0800

    2019/7/7/1801

--- loop/src/circulate/Guess_number.java
+++ loop/src/circulate/Guess_number.java
@@ -7,20 +7,19 @@ public class Guess_number {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		//[0,1)-->[0,100)-->[1,100]
+		// [0,1)-->[0,100)-->[1,100]
 		int number = (int) (Math.random() * 100 + 1);
 		int a;
-		int count=0;
+		int count = 0;
 		do {
-			a=in.nextInt();
-			count=count+1;
-			if (a>number) {
+			a = in.nextInt();
+			count = count + 1;
+			if (a > number) {
 				System.out.println("偏大");
 			} else {
 				System.out.println("偏小");
 			}
-		} while (a!=number);
-		System.out.println("恭喜你猜对了，你猜了"+count+"次");
+		} while (a != number);
+		System.out.println("恭喜你猜对了，你猜了" + count + "次");
 	}
-
 }
