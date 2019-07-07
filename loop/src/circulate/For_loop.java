a6e7d7c3f04da222475fd17172b3df4ee375f2a8
Alexluwh <35101826+Alexluwh@users.noreply.github.com>
2019-07-07 21:13:14 +0800

    2019/7/7/2113

---
+++ loop/src/circulate/For_loop.java
@@ -0,0 +1,16 @@
+package circulate;
+
+import java.util.Scanner;
+
+/**
+ * For_loop
+ */
+public class For_loop {
+
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       int i=1;
+       int factor =1;
+       
+    }
+}
